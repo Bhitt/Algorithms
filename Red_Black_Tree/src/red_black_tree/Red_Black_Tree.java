e83b44383f0c7e9d11f9ab766cedd938a75cac8e
Bhitt <BrandenHitt@gmail.com>
2019-01-09 16:43:12 -0800

    +some redblack tree stuff (needs code still)
    Signed-off-by: Bhitt <BrandenHitt@gmail.com>

---
+++ Red_Black_Tree/src/red_black_tree/Red_Black_Tree.java
@@ -0,0 +1,35 @@
+/*
+    RED BLACK TREES
+-every node is RED or BLACK
+-root is always BLACK
+-new insertions are always RED
+-every path from root - leaf has the same nummber of BLACK nodes
+-no path can have two consecutive RED nodes
+-nulls are BLACK
+
+Rebalance:
+    BLACK aunt rotate
+    RED aunt colorflip
+    
+    After rotation:           After color flip:
+         BLACK                      RED
+         /   \                     /   \
+        RED   RED               BLACK  BLACK
+*/
+package red_black_tree;
+
+/**
+ *
+ * @author bhitt
+ */
+public class Red_Black_Tree {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //
+        
+    }
+    
+}
