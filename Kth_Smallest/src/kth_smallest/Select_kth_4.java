c3e8d49e13c4d2185bfb932cde731ddc00f54d8c
bphitt <BrandenHitt@gmail.com>
2019-04-28 15:44:46 -0700

    +finished program, just need touchups and testing added
    Signed-off-by: bphitt <BrandenHitt@gmail.com>

--- Kth_Smallest/src/kth_smallest/Select_kth_4.java
+++ Kth_Smallest/src/kth_smallest/Select_kth_4.java
@@ -12,6 +12,7 @@ package kth_smallest;
 public class Select_kth_4 {
     
     int Select2(int[] arr, int n, int k){
+        //Each subset will be of size r
         int r=5;
         //if n <= r then sort arr and return the kth element 
         if(n<=r){
@@ -21,78 +22,72 @@ public class Select_kth_4 {
         //divide arr into n/r subsets of size r each, ignoring excess elements
         //and let M be the set of medians
         int temp[] = new int[r];
-        int M[] = new int[n/r];
-        for(int i=0;i<(n/r);i++){
+        int M[] = new int[((int)Math.floor(n/r))];
+        for(int i=0;i<M.length;i++){
             //copy out a subset of size r
             for(int j=0;j<r;j++){
-                temp[j] = arr[i*j];
+                temp[j] = arr[(i*r)+j];
             }
             //sort the subset
             insertionSort(temp);
             //add the median of the subset into M
-            M[i] = temp[r/2];
-        }        
+            M[i] = temp[((int)Math.floor(r/2))-1];
+        }
         
         //find the median of medians
-        int v = Select2(M,n/r,(n/r)/2);
-        System.out.println("v:"+v);
+        int v = Select2(M,(int)Math.floor(n/r),((((int)Math.floor(n/r))/2)+1));
         
         //partition using the median of medians as the pivot
         int pivotPos = 0;
-        for(int i=0;i<n;i++){
-            //System.out.println("arr[i]:"+i);
+        for(int i=0;i<arr.length;i++){
             if(arr[i]==v) pivotPos=i;
         }
         int m = 0;
         int j = n-1;
         pivotPos = partition(arr,m,j,pivotPos);
         
-        System.out.println("pivotPos: "+pivotPos);
-        
         //check for kth element
         if(k == (pivotPos+1)){
+            //if the pivot is at the kth position, return the kth element
             return arr[pivotPos];
         }else if(k < (pivotPos+1)){
+            //If the pivot is greater than k, call Select2 on the bottom half
+            //      of the array below the pivot.
             //let S be the set of elements arr(1,...,pivotPos-1)
             int[] S = new int[pivotPos];
-            for(int i=0;i<(pivotPos);i++){
+            for(int i=0;i<S.length;i++){
                 S[i] = arr[i];
             }
-            return Select2(S,pivotPos,k);
+            return Select2(S,S.length,k);
         }else{
+            //If the pivot is less than k, call Select2 on the top half
+            //      of the array below the pivot.
             //let R be the set of element arr(pivotPos+1,...,n)
             int[] R = new int[n-(pivotPos+1)];
             for(int i=0;i<(n-(pivotPos+1));i++){
-                R[i]=arr[pivotPos+i];
+                R[i]=arr[pivotPos+1+i];
             }
-            return Select2(R,(n-(pivotPos+1)),k-(pivotPos+1));
+            return Select2(R,R.length,k-(pivotPos+1));
         }
     }
     
-    int partition(int arr[], int low, int high, int pivot) 
-    {   
-        int i = (low-1); // index of smaller element 
-        for (int j=low; j<high; j++) 
-        { 
-            // If current element is smaller than or 
-            // equal to pivot 
-            if (arr[j] <= pivot) 
-            { 
-                i++; 
-  
-                // swap arr[i] and arr[j] 
-                int temp = arr[i]; 
-                arr[i] = arr[j]; 
-                arr[j] = temp; 
-            } 
-        } 
-  
-        // swap arr[i+1] and arr[high] (or pivot) 
-        int temp = arr[i+1]; 
-        arr[i+1] = arr[high]; 
-        arr[high] = temp; 
-  
-        return i+1; 
+     int partition(int[] array, int low, int high, int pivot){
+        //
+        swap(array, low, pivot);
+        int border = low +1;
+        for(int i=border; i<= high; i++){
+            if(array[i] < array[low]){
+                swap(array, i , border++);
+            }
+        }
+        swap(array, low, border-1);
+        return border-1;
+    }
+     
+    void swap(int[] array, int index1, int index2){
+        int temp =  array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
     }
     
     void insertionSort(int arr[]){
@@ -109,5 +104,12 @@ public class Select_kth_4 {
             } 
             arr[j + 1] = key; 
         } 
+    }
+    
+    void printArray(int[] arr,int size){
+        for(int i=0;i<size;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
